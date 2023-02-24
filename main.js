e9c6b7e01bdd2fa8a5d297ada15a235458252386
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-02-24 17:19:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const btn = document.getElementsByClassName("booking-data__section-side-title")[1];
-btn.innerHTML ="Reservar";
-        
+document.addEventListener("DOMContentLoaded", function(event) {
+    //  const btn = document.getElementsByClassName("booking-data__section-side-title")[1];
+    // btn.innerHTML ="Reservar";
+
+const nuevaCaja = document.getElementsByClassName("nav-elem")[3];
+    nuevaCaja.href = "/packages/flighthotel/";
+ });
+
+console.log(btn);
+
+
+
 
