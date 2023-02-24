c48b0369b50842eec06a1be285c700ed36b9356f
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-02-24 16:43:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-            const btn = document.getElementsByClassName("booking-data__section-side-title")[1];
-            btn.innerHTML ="Reservar";
-          });
+const btn = document.getElementsByClassName("booking-data__section-side-title")[1];
+btn.innerHTML ="Reservar";
+        
 
-console.log(btn);
